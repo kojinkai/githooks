d270b5de6c35a41a28fe0055aa96b11cb392b645
Lewis Nixon <lewcfn@gmail.com>
2017-07-19 10:21:10 +0100

    feat(commit-msg): Add a Regex for checking standard message formatting

--- commit-msg.sh
+++ commit-msg.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-# Check that the commit message contains a particular string, e.g. JIRA ticket number
+# Check that the commit message is correctly formatted
+#
+# Example Passing message:
+# feat(module): JIRA-123 Add a new feature
+#
+# Example failures:
+# chore(module):JIRA-123 Add a new feature
+# chore(module): JIRA-123 add a new feature
+# chore(): JIRA-123 add a new feature
 
-test "" != "$(grep 'JIRAPROJECT-' "$1")" || {
+regex='^(feat|fix|docs|style|refactor|perf|test|chore)\([a-z]+-?[a-z]+\):{1} JIRA-[0-9]+ ([A-Z].*)'
+
+test "" != "$(grep "$regex" "$1")" || {
   echo >&2 "Your message when the test above fails"
   exit 1
 }
